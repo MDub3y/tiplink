cecadaec307eceefc400bcc5953db65852192cd4
Awesxme <manasvi.d007@gmail.com>
2026-05-02 18:05:16 +0000

    signin

--- backend/src/handlers/auth.rs
+++ backend/src/handlers/auth.rs
@@ -1,27 +1,31 @@
-use actix_web::{web, HttpResponse, Responder};
-use argon2::password_hash::SaltString;
-use crate::entities::{prelude::*, *};
 use crate::AppState;
-use sea_orm::*;
-use uuid::Uuid;
+use crate::entities::{prelude::*, *};
+use actix_web::{HttpResponse, Responder, cookie::Cookie, cookie::time::Duration, web};
+use argon2::password_hash::SaltString;
 use argon2::{
-    password_hash::{ rand_core::OsRng, PasswordHasher},
     Argon2,
+    password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, rand_core::OsRng},
 };
+use chrono::{Duration as ChronoDuration, Utc};
+use sea_orm::*;
 use serde::Deserialize;
+use uuid::Uuid;
 
 #[derive(Deserialize)]
 pub struct SignupRequest {
     pub email: String,
     pub password: String,
     pub username: String,
-    pub asset_password: String
+    pub asset_password: String,
 }
 
-pub async fn signup (
-    data: web::Data<AppState>,
-    req: web::Json<SignupRequest>,
-) -> impl Responder {
+#[derive(Deserialize)]
+pub struct SigninRequest {
+    pub email: String,
+    pub password: String,
+}
+
+pub async fn signup(data: web::Data<AppState>, req: web::Json<SignupRequest>) -> impl Responder {
     let db = &data.db;
 
     let salt = SaltString::generate(&mut OsRng);
@@ -61,22 +65,14 @@ pub async fn signup (
         .await
         .unwrap();
 
-        let available_wallet = Balances::find()
-        .filter(balances::Column::UserId.is_null())
-        .one(&txn)
-        .await
-        .unwrap();
-
     match available_wallet {
         Some(wallet) => {
             let pubkey = wallet.pubkey.clone();
-            
-            // Link wallet to user
+
             let mut wallet_active: balances::ActiveModel = wallet.into();
             wallet_active.user_id = Set(Some(user_id));
             wallet_active.update(&txn).await.unwrap();
 
-            // 5. Store the Asset Password Hash linked to this specific pubkey
             let asset_pwd = asset_password::ActiveModel {
                 pubkey: Set(pubkey.clone()),
                 hash: Set(asset_hash),
@@ -96,4 +92,76 @@ pub async fn signup (
             HttpResponse::InternalServerError().body("No pre-generated wallets available!")
         }
     }
+}
+
+pub async fn signin(data: web::Data<AppState>, req: web::Json<SigninRequest>) -> impl Responder {
+    let db = &data.db;
+
+    let user = match Users::find()
+        .filter(users::Column::Email.eq(req.email.clone()))
+        .one(db)
+        .await
+        .unwrap()
+    {
+        Some(u) => u,
+        None => return HttpResponse::Unauthorized().body("Invalild email or password"),
+    };
+
+    let parsed_hash = match PasswordHash::new(&user.password_hash) {
+        Ok(h) => h,
+        Err(_) => return HttpResponse::InternalServerError().body("Error parsing stored hash"),
+    };
+
+    if let Err(_) = Argon2::default().verify_password(req.password.as_bytes(), &parsed_hash) {
+        return HttpResponse::Unauthorized().body("Invalid Email or Password");
+    }
+
+    let expiry = Utc::now() + ChronoDuration::hours(24);
+    let timestamp = expiry.timestamp();
+    let uuid = Uuid::new_v4().to_string();
+
+    let composite_token = format!("{}:{}", timestamp, uuid);
+
+    let username = user.username.clone();
+
+    let mut user_active: users::ActiveModel = user.into();
+    user_active.cookie = Set(Some(composite_token.clone()));
+
+    if let Err(_) = user_active.update(db).await {
+        return HttpResponse::InternalServerError().body("Failed to create session");
+    }
+
+    let auth_cookie = Cookie::build("session_token", composite_token)
+        .path("/")
+        .http_only(true)
+        .max_age(Duration::days(1))
+        .finish();
+
+    HttpResponse::Ok()
+        .cookie(auth_cookie)
+        .json(serde_json::json!({
+            "status": "success",
+            "username": username
+        }))
+}
+
+pub async fn check_session(db: &DatabaseConnection, token: String) -> Option<users::Model> {
+    let user = Users::find()
+        .filter(users::Column::Cookie.eq(token.clone()))
+        .one(db)
+        .await
+        .ok()??;
+
+    let parts: Vec<&str> = token.split(':').collect();
+    if parts.len() != 2 {
+        return None;
+    }
+
+    let expiry_timestamp: i64 = parts[0].parse().ok()?;
+
+    if Utc::now().timestamp() > expiry_timestamp {
+        return None;
+    }
+
+    Some(user)
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,6 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use sea_orm::{Database, DatabaseConnection};
+use actix_web::{App, HttpResponse, HttpServer, Responder, web};
 use dotenvy::dotenv;
+use sea_orm::{Database, DatabaseConnection};
 use std::env;
 
 mod entities;
@@ -15,7 +15,9 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL not set");
 
-    let db: DatabaseConnection = Database::connect(db_url).await.expect("Failed to connect to DB!");
+    let db: DatabaseConnection = Database::connect(db_url)
+        .await
+        .expect("Failed to connect to DB!");
 
     println!("Tiplink Gateway connected to postgres");
 
@@ -23,6 +25,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(web::Data::new(AppState { db: db.clone() }))
             .route("/signup", web::post().to(handlers::auth::signup))
+            .route("/signin", web::post().to(handlers::auth::signin))
             .route("/health", web::get().to(health_check))
     })
     .bind(("127.0.0.1", 8080))?
